a6dd38cfec6f0be443a06cab65d6ba5a5228ca03
omerfarukdoganweb <omerfarukdoganweb@gmail.com>
2022-01-07 21:26:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,44 @@
-# -*- coding: utf-8 -*-
-
 import urllib.request
 from fpdf import FPDF
 from bs4 import BeautifulSoup
 
-#Bu kısımda mevcut slayta ait source kodları çekiyoruz.
-url=input("Lütfen İndirmek istediğiniz slayt dosyasının bulunduğu web urlsini yazın:")
+url=input("slide url:")
 response=urllib.request.urlopen(url)
 data=response.read()
 response.close()
 liste=[]
-#elde ettiğimiz dosyayı aşağıda parse ediyoruz ve gerekli olan kısımları Liste adındaki listede tutuyoruz.
+
 soup=BeautifulSoup(data,"html.parser")
 for i in soup.find_all("img",attrs={"class":"slide_image"}):
     liste.append(i["data-full"])
-sayaç=0
+x=0
 
-#benim yaptığım örnek 300 sayfalı bir slayt idi onda hatasız bir şekilde ilerledi.
-#liste tam istediğim şekilde oluştu o yüzden ekstra işlem yapmama gerek kalmadı 300 sayısınıda listenin len özelliğini kullanrak aldım
-#indirdiğim resimleri mevcut dizine kayıt ettirdim.
 for i in liste:
-    sayaç=sayaç+1
+    x=x+1
     image=urllib.request.urlopen(i)
     byte=image.read()
     image.close()
-    f=open(str(sayaç)+".jpg","wb")
+    f=open(str(x)+".jpg","wb")
     f.write(byte)
     f.close()
-    print(sayaç,"nolu sayfa bitti.")
+    print(x)
     
-
-#burasıda pdf haline getirdiğim kısım zaten basit
-sayaç=0
+x=0
 imagelist=[]
 pdf = FPDF()
-while sayaç<int(len(liste)):
-    sayaç=sayaç+1
-    imagelist.append(str(sayaç)+".jpg")
+while x<int(len(liste)):
+    x=x+1
+    imagelist.append(str(x)+".jpg")
 
-print("Bir kısım İşlem tamamlandı.")
+print("wait")
 
 for image in imagelist:
     pdf.add_page("P")
-    pdf.image(image,10,10,190,278)#şu kısımda rakamlar tamamen deneme yanılmayla buldum görsele göre ayarlamak lazım.
+    pdf.image(image,10,10,190,278)
     print(image," bitti")
 
-print("PDF Çıktısı oluşturuluyor Lütfen bekleyin.")
+print("preparing pdf")
 
-pdf.output("YeniPDF.pdf", "F")
+pdf.output("file.pdf", "F")
 
-print("bitti")
+print("ok")
